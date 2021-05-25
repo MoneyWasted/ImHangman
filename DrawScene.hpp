474252d79310c2c97720f948accc74554bf787fc
MoneyWasted <45946092+MoneyWasted@users.noreply.github.com>
2021-05-25 09:41:43 -0400

    Minor Improvements

--- DrawScene.hpp
+++ DrawScene.hpp
@@ -9,7 +9,7 @@
 #define WHITE ImColor(255, 255, 255, 255)
 #define RED ImColor(255, 0, 0, 255)
 
-bool debug = false;
+bool debug = true;
 bool game = true;
 
 Renderer* RENDERER = new Renderer;
@@ -17,6 +17,7 @@ std::optional<string> RandWord = RandomWord();
 
 const char* message = "";
 char letter;
+std::size_t found;
 int NumWrongGuesses = 0;
 
 // Initialize the secret word with the * character.
@@ -53,22 +54,25 @@ void DrawScene()
 		// If a key 'A' through 'Z' is pressed.
 		if (ImGui::IsKeyPressed(letter))
 		{
-			// iterate over the characters in word and guess
-			for (size_t i = 0; i < sizeof(word.c_str()); i++)
+			found = HiddenWord.find(letter, 0);
+			if (found != std::string::npos)
 			{
-				if (word.c_str()[i] == tolower(letter))
-				{
-					HiddenWord[i] = word[i];  // if the characters match at position i, update the underscore.
-					message = "You found a letter! Isn't that exciting!";
-					wrong = false;
-					break;
-				}
-				else
-				{
-					wrong = true;
-					message = "Whoops! That letter isn't in there!";
-				}
+				continue;
 			}
+			found = word.find(letter, 0);
+			if (found == std::string::npos)
+			{
+				wrong = true;
+				message = "Whoops! That letter isn't in there!";
+			}
+			while (found != std::string::npos)
+			{
+				guessWord[found] = answer;
+				found = secretWord.find(answer, found + 1);
+			}
+
+			message = "You found a letter! Isn't that exciting!";
+			message = "Whoops! That letter isn't in there!";
 		}
 	}
 
@@ -81,38 +85,34 @@ void DrawScene()
 		message = "You Won!";
 	}
 
-	if (NumWrongGuesses == 1)
+	switch (NumWrongGuesses)
 	{
+	case 1:
 		RENDERER->DrawFilledCircle(ImVec2(305, 125), 50, 50.f, WHITE); // Head
-	}
-	else if (NumWrongGuesses == 2)
-	{
+		break;
+	case 2:
 		RENDERER->DrawFilledCircle(ImVec2(305, 125), 50, 50.f, WHITE); // Head
 		RENDERER->DrawLine(ImVec2(305, 125), ImVec2(305, 300), WHITE, 10); // Body
-	}
-	else if (NumWrongGuesses == 3)
-	{
+		break;
+	case 3:
 		RENDERER->DrawFilledCircle(ImVec2(305, 125), 50, 50.f, WHITE); // Head
 		RENDERER->DrawLine(ImVec2(305, 125), ImVec2(305, 300), WHITE, 10); // Body
 		RENDERER->DrawLine(ImVec2(305, 300), ImVec2(255, 350), WHITE, 10); // Right Leg
-	}
-	else if (NumWrongGuesses == 4)
-	{
+		break;
+	case 4:
 		RENDERER->DrawFilledCircle(ImVec2(305, 125), 50, 50.f, WHITE); // Head
 		RENDERER->DrawLine(ImVec2(305, 125), ImVec2(305, 300), WHITE, 10); // Body
 		RENDERER->DrawLine(ImVec2(305, 300), ImVec2(255, 350), WHITE, 10); // Right Leg
 		RENDERER->DrawLine(ImVec2(305, 300), ImVec2(355, 350), WHITE, 10); // Left Leg
-	}
-	else if (NumWrongGuesses == 5)
-	{
+		break;
+	case 5:
 		RENDERER->DrawFilledCircle(ImVec2(305, 125), 50, 50.f, WHITE); // Head
 		RENDERER->DrawLine(ImVec2(305, 125), ImVec2(305, 300), WHITE, 10); // Body
 		RENDERER->DrawLine(ImVec2(305, 300), ImVec2(255, 350), WHITE, 10); // Right Leg
 		RENDERER->DrawLine(ImVec2(305, 300), ImVec2(355, 350), WHITE, 10); // Left Leg
 		RENDERER->DrawLine(ImVec2(305, 200), ImVec2(255, 250), WHITE, 10); // Right Arm
-	}
-	else if (NumWrongGuesses >= 6)
-	{
+		break;
+	case 6:
 		RENDERER->DrawFilledCircle(ImVec2(305, 125), 50, 50.f, RED); // Head
 		RENDERER->DrawLine(ImVec2(305, 125), ImVec2(305, 300), RED, 10); // Body
 		RENDERER->DrawLine(ImVec2(305, 300), ImVec2(255, 350), RED, 10); // Right Leg
@@ -120,8 +120,11 @@ void DrawScene()
 		RENDERER->DrawLine(ImVec2(305, 200), ImVec2(255, 250), RED, 10); // Right Arm
 		RENDERER->DrawLine(ImVec2(305, 200), ImVec2(355, 250), RED, 10); // Left Arm
 		message = "You have lost!";
+		break;
+	default:
+		message = "Press a key to get started.";
+		break;
 	}
 
-		
 	RENDERER->DrawTextW(ImVec2(450, 200), WHITE, HiddenWord.c_str());
 }
